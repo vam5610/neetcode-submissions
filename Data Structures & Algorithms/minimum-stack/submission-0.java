51d89b6b4edabe235305254b5d0fa1718ea3633c
Vamsi <karthikvamsivamsi@gmail.com>
2026-06-21 14:35:27 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,36 @@
+class MinStack {
+
+    private Stack<Integer> stack;
+
+    public MinStack() {
+        stack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+    }
+
+    public void pop() {
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        Stack<Integer> tmp = new Stack<>();
+        int mini = stack.peek();
+
+        while (!stack.isEmpty()) {
+            mini = Math.min(mini, stack.peek());
+            tmp.push(stack.pop());
+        }
+
+        while (!tmp.isEmpty()) {
+            stack.push(tmp.pop());
+        }
+
+        return mini;
+    }
+}
